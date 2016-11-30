33c3132f1d2964e06b07d45464520b526ede3c9e
Zoran Heinczinger <heinczingerz@5net.hu>
2016-11-30 17:15:21 +0100

    Fill the contact form with valid information

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,12 +1,11 @@
 <?php
-// Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-   echo "No arguments Provided!";
+   echo "Hiányzó adat, kérjük ellenőrizze!";
    return false;
    }
    
@@ -15,12 +14,11 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$to = 'info@kozmetikustimi.hu';
+$email_subject = "Új üzenet érkezett a honlapról:  $name részéről";
+$email_body = "Üzeneted érkezett a honlapodon található űrlapból.\n\n"."A részleteket itt olvashatod:\n\nNév: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nÜzenet:\n$message";
+$headers = "From: info@kozmetikustimi.hu\n";
+$headers .= "Válasz: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
